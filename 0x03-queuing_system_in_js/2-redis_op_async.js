c8900e5386d2c1040a1d93b49e28e333c71f34a5
rational-being <overcomer021@gmail.com>
2024-06-11 14:44:44 +0100

    basic Redis-based queuing system with a process to generate job and a second one to process it

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+#!/usr/bin/yarn dev
+import { createClient } from "redis";
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server:')
+});
+
+const setNewSchool = (schoolName, value) => {
+    client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+    console.log(await promisify(client,GET).bind(client)(schoolName));
+    // client.length(schoolName, (_err, reply) => {
+    //     console.log(reply);
+    // });
+};
+
+async function main() {
+    await displaySchoolValue('Holberton');
+    setNewSchool('HolbertonSanFrancisco', "100");
+    await displaySchoolValue("HolbertonSanFrancisco");
+}
+
+client.on('connect', async () => {
+    console.log('Redis client connected to the server');
+    await.main();
+});
---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,40 @@
+#!/usr/bin/yarn dev
+import { hash } from "crypto";
+import { createClient } from "redis";
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server:')
+});
+
+const updateHash = (hashName, fieldName, fieldValue) => {
+    client.HSET(hashName, fieldName, fieldValue, print);
+};
+
+const printHash = (hashName) => {
+    client.HGETALL(hashName, (_err, reply) => console.log(reply));
+};
+
+function main() {
+    const hashObj = {
+        Portland: 50,
+        Seattle: 80,
+        'New York': 20,
+        Bogota: 20,
+        Cali: 40,
+        Paris: 2,
+    };
+
+    for (const [filed, value] of Object.entries(hashObj)) {
+        updateHash('HolbertonSchools', field, value);
+    }
+    printHash('HolbertonSchools');
+}
+
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+    main();
+});
